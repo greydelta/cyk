18dcea8986a6dadaf5dd5e1ab447e4a829c4023a
aw <greydelta77@gmail.com>
2023-01-05 23:45:22 +0800

    add: utility methods

--- src/ConsoleUI.java
+++ src/ConsoleUI.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.io.*;
 
 public class ConsoleUI {
 
@@ -22,14 +24,69 @@ public class ConsoleUI {
         return control;
     }
 
-    public void loadAllData() {
+    public void launch() {
     }
 
-    public void launch() {
-        System.out.println("Chomsky program");
-        System.out.println("================");
-        System.out.println(">>>> START");
 
-        System.out.println(">>>> END");
+    /*
+     * =======================================
+     * ====== SECTION : Utility Methods ======
+     * =======================================
+     */
+    public int intInputValidation(int lower, int upper) throws IllegalArgumentException {
+        setScanner(new Scanner(System.in));
+        int userInput;
+
+        if (scan.hasNextLine()) {
+            String tempString = scan.nextLine();
+
+            // Check if blank / only whitespace
+            if (tempString.isEmpty() == true)
+                throw new IllegalArgumentException("\nNot allowed to enter blank values!");
+            else
+
+                // Check if is valid integer
+                try {
+                    // Convert to integer
+                    userInput = Integer.parseInt(tempString);
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("\nEnter integers Only!");
+                }
+
+            // Check if range is same, then can only enter that value
+            if (lower == upper)
+                if (userInput != lower)
+                    throw new IllegalArgumentException("\nEnter " + lower + " only!");
+                else
+                    return userInput;
+
+            // Check if it is within range
+            if (userInput < lower || userInput > upper)
+                throw new IllegalArgumentException("\nEnter values between " + lower + " and " + upper + " only!");
+            else
+                return userInput;
+        } else
+            throw new IllegalArgumentException("\nEnter integers Only!");
+    }
+
+    public String stringInputValidation() throws IllegalArgumentException {
+        setScanner(new Scanner(System.in));
+        String userInput;
+        if (scan.hasNextLine()) {
+            userInput = scan.nextLine();
+            if (userInput.isEmpty() == true || userInput.trim().length() < 1 == true)
+                throw new IllegalArgumentException("Not allowed to enter blank values!");
+            else
+                return userInput;
+        } else
+            throw new IllegalArgumentException("Scanner is closed");
+    }
+
+    public void bufferFor5Miliseconds() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e1) {
+            System.err.print(e1.getMessage());
+        }
     }
 }
