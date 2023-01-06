7cff1760e306a91b3891e905ca6a37694a68d1ff
aw <greydelta77@gmail.com>
2023-01-06 16:18:00 +0800

    add: canGenerateVariable flag to Grammar class

--- src/Grammar.java
+++ src/Grammar.java
@@ -3,15 +3,18 @@ import java.util.*;
 public class Grammar {
     private List<String> variable;
     private String startVariable;
+    private boolean canGenerateVariable;
 
     public Grammar() {
         this.variable = null;
         this.startVariable = "";
+        this.canGenerateVariable = false;
     }
 
-    public Grammar(List<String> variable, String startVariable) {
+    public Grammar(List<String> variable, String startVariable, boolean canGenerateVariable) {
         this.variable = variable;
         this.startVariable = startVariable;
+        this.canGenerateVariable = canGenerateVariable;
     }
 
     public List<String> getVariable() {
@@ -22,12 +25,20 @@ public class Grammar {
         return this.startVariable;
     }
 
+    public boolean getCanGenerateVariable() {
+        return this.canGenerateVariable;
+    }
+
     public void setVariable(List<String> variable) {
         this.variable = variable;
     }
 
     public void setStartVariable(String startVariable) {
         this.startVariable = startVariable;
+    }
+
+    public void setCanGenerateVariable(boolean canGenerateVariable) {
+        this.canGenerateVariable = canGenerateVariable;
     }
 
     public void addVariable(String variable) {
